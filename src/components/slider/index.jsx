0ec02ed56166b6b211f12213fdc864a30f25ca2e
xiuxiuyifan <1575243732@qq.com>
2023-04-04 22:10:39 +0800

    feat: 轮播组件开发完成

---
+++ src/components/slider/index.jsx
@@ -0,0 +1,27 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
+import { SliderContainer } from "./style.js";
+
+const Slider = (props) => {
+  const { bannerList } = props;
+  return (
+    <SliderContainer>
+      <div className="before"></div>
+      <div className="slider-container">
+        <Swiper pagination={true} modules={[Pagination]} loop={true}>
+          {bannerList.map((slider, index) => (
+            <SwiperSlide key={index} className="swiper-slide">
+              <div className="slider-nav">
+                <img src={slider.imgUrl} width="100%" height="100%" />
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </SliderContainer>
+  );
+};
+
+export default Slider;
--- src/pages/Recommend/index.jsx
+++ src/pages/Recommend/index.jsx
@@ -1,4 +1,5 @@
 import { memo } from "react";
+import Slider from "../../components/slider";
 
 function Recommend() {
   const bannerList = [1, 2, 3, 4].map((item) => {
@@ -6,7 +7,11 @@ function Recommend() {
       imgUrl: `http://p1.music.126.net/ZYLJ2oZn74yUz5x8NBGkVA==/109951164331219056.jpg`,
     };
   });
-  return <div></div>;
+  return (
+    <div>
+      <Slider bannerList={bannerList}></Slider>
+    </div>
+  );
 }
 
 export default memo(Recommend);

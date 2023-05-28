4e7299571b84f312fb1abe8371e973f9e3dadb61
xiuxiuyifan <1575243732@qq.com>
2023-05-28 20:09:09 +0800

    feat: 搜索组件基本完成

---
+++ src/baseUI/search-box/index.jsx
@@ -0,0 +1,98 @@
+import { memo, useEffect, useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import style from "../../assets/global-style";
+import styled from "styled-components";
+import { debounce } from "../../utils";
+
+const SearchBoxWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  box-sizing: border-box;
+  width: 100%;
+  padding: 0 6px;
+  padding-right: 20px;
+  height: 40px;
+  background: ${style["theme-color"]};
+  .icon-back {
+    font-size: 24px;
+    color: ${style["font-color-light"]};
+  }
+  .box {
+    flex: 1;
+    margin: 0 5px;
+    line-height: 18px;
+    background: ${style["theme-color"]};
+    color: ${style["highlight-background-color"]};
+    font-size: ${style["font-size-m"]};
+    outline: none;
+    border: none;
+    border-bottom: 1px solid ${style["border-color"]};
+    &::placeholder {
+      color: ${style["font-color-light"]};
+    }
+  }
+  .icon-delete {
+    font-size: 16px;
+    color: ${style["background-color"]};
+  }
+`;
+
+const SearchBox = (props) => {
+  const [query, setQuery] = useState("");
+
+  const queryRef = useRef();
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate(-1);
+  };
+
+  // 父组件热门搜索拿到的新关键词
+  const { newQuery } = props;
+
+  // 触发搜索事件后的处理逻辑，从外部传入
+  const { handleQuery } = props;
+
+  const handleChange = (e) => {
+    let value = e.currentTarget.value;
+    // 调用父组件的回调方法
+    debounce(handleQuery)(query);
+    setQuery(value);
+  };
+
+  const clearQuery = () => {
+    setQuery("");
+    queryRef.current.focus();
+  };
+
+  const displayStyle = query ? { display: "block" } : { display: "none" };
+
+  // 进场的时候光标选中
+  useEffect(() => {
+    queryRef.current.focus();
+  }, []);
+
+  return (
+    <SearchBoxWrapper>
+      <i className="iconfont icon-back" onClick={() => goBack()}>
+        &#xe655;
+      </i>
+      <input
+        ref={queryRef}
+        className="box"
+        placeholder="搜索歌曲、歌手、专辑"
+        value={query}
+        onChange={handleChange}
+      />
+      <i
+        className="iconfont icon-delete"
+        onClick={clearQuery}
+        style={displayStyle}
+      >
+        &#xe600;
+      </i>
+    </SearchBoxWrapper>
+  );
+};
+
+export default memo(SearchBox);
--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -1,12 +1,15 @@
-import { memo, useEffect, useState } from "react";
+import { memo, useCallback, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { CSSTransition } from "react-transition-group";
 import { Container } from "./style";
+import SearchBox from "../../baseUI/search-box";
 
 const Search = () => {
   const [show, setShow] = useState(false);
 
   const navigate = useNavigate();
+
+  const [query, setQuery] = useState("");
 
   const goBack = () => {
     navigate(-1);
@@ -15,6 +18,14 @@ const Search = () => {
   useEffect(() => {
     setShow(true);
   }, []);
+
+  const searchBack = useCallback(() => {
+    setShow(false);
+  }, []);
+
+  const handleQuery = (q) => {
+    setQuery(q);
+  };
 
   return (
     <CSSTransition
@@ -25,7 +36,13 @@ const Search = () => {
       onExit={() => goBack()}
     >
       <Container>
-        <div onClick={() => setShow(false)}>返回</div>
+        <div className="search_box_wrapper">
+          <SearchBox
+            back={searchBack}
+            newQuery={query}
+            handleQuery={handleQuery}
+          ></SearchBox>
+        </div>
       </Container>
     </CSSTransition>
   );
